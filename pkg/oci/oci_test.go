944e895a9b4431c87e095808c31b0ce199d07435
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2026-09-08 19:15:28 -0600

    Fix build and tests after go-vex and packageurl-go bumps
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- pkg/oci/oci_test.go
+++ pkg/oci/oci_test.go
@@ -23,10 +23,10 @@ func TestGenerateReferenceIdentifiers(t *testing.T) {
 			expected: IdentifiersBundle{
 				Identifiers: map[vex.IdentifierType][]string{
 					vex.PURL: {
-						"pkg:oci/alpine@sha256%3Aeece025e432126ce23f223450a0326fbebde39cdf496a85d8c016293fc851978?repository_url=index.docker.io%2Flibrary%2Falpine",
-						"pkg:oci/alpine@sha256%3Aeece025e432126ce23f223450a0326fbebde39cdf496a85d8c016293fc851978?arch=amd64&os=linux&repository_url=index.docker.io%2Flibrary%2Falpine",
-						"pkg:oci/alpine@sha256%3A48d9183eb12a05c99bcc0bf44a003607b8e941e1d4f41f9ad12bdcc4b5672f86?repository_url=index.docker.io%2Flibrary%2Falpine",
-						"pkg:oci/alpine@sha256%3A48d9183eb12a05c99bcc0bf44a003607b8e941e1d4f41f9ad12bdcc4b5672f86?arch=amd64&os=linux&repository_url=index.docker.io%2Flibrary%2Falpine",
+						"pkg:oci/alpine@sha256:eece025e432126ce23f223450a0326fbebde39cdf496a85d8c016293fc851978?repository_url=index.docker.io%2Flibrary%2Falpine",
+						"pkg:oci/alpine@sha256:eece025e432126ce23f223450a0326fbebde39cdf496a85d8c016293fc851978?arch=amd64&os=linux&repository_url=index.docker.io%2Flibrary%2Falpine",
+						"pkg:oci/alpine@sha256:48d9183eb12a05c99bcc0bf44a003607b8e941e1d4f41f9ad12bdcc4b5672f86?repository_url=index.docker.io%2Flibrary%2Falpine",
+						"pkg:oci/alpine@sha256:48d9183eb12a05c99bcc0bf44a003607b8e941e1d4f41f9ad12bdcc4b5672f86?arch=amd64&os=linux&repository_url=index.docker.io%2Flibrary%2Falpine",
 					},
 				},
 				Hashes: map[vex.Algorithm][]vex.Hash{
@@ -44,8 +44,8 @@ func TestGenerateReferenceIdentifiers(t *testing.T) {
 			expected: IdentifiersBundle{
 				Identifiers: map[vex.IdentifierType][]string{
 					vex.PURL: {
-						"pkg:oci/curl@sha256%3A3b987bc327e8aa8e7db26822e0552d927d25392ccb4d3b9d30b5390b485520d8?repository_url=cgr.dev%2Fchainguard%2Fcurl",
-						"pkg:oci/curl@sha256%3A3b987bc327e8aa8e7db26822e0552d927d25392ccb4d3b9d30b5390b485520d8?arch=amd64&os=linux&repository_url=cgr.dev%2Fchainguard%2Fcurl",
+						"pkg:oci/curl@sha256:3b987bc327e8aa8e7db26822e0552d927d25392ccb4d3b9d30b5390b485520d8?repository_url=cgr.dev%2Fchainguard%2Fcurl",
+						"pkg:oci/curl@sha256:3b987bc327e8aa8e7db26822e0552d927d25392ccb4d3b9d30b5390b485520d8?arch=amd64&os=linux&repository_url=cgr.dev%2Fchainguard%2Fcurl",
 					},
 				},
 				Hashes: map[vex.Algorithm][]vex.Hash{
@@ -62,10 +62,10 @@ func TestGenerateReferenceIdentifiers(t *testing.T) {
 			expected: IdentifiersBundle{
 				Identifiers: map[vex.IdentifierType][]string{
 					vex.PURL: {
-						"pkg:oci/scanner-test@sha256%3Aa0acdc09bf31ac45076a5850d0564ee144a9c8483f89eb869dc6ddd6218629c9?repository_url=localhost%3A5000%2Fscanner-test",
-						"pkg:oci/scanner-test@sha256%3Aa0acdc09bf31ac45076a5850d0564ee144a9c8483f89eb869dc6ddd6218629c9?arch=amd64&os=linux&repository_url=localhost%3A5000%2Fscanner-test&tag=unfixed-vulnerabilities-wolfi",
-						"pkg:oci/scanner-test@sha256%3A86e3ee69ede5f56514972d16fcfcfafbb54a65d2349c6f8419670bf7993d8583?repository_url=localhost%3A5000%2Fscanner-test",
-						"pkg:oci/scanner-test@sha256%3A86e3ee69ede5f56514972d16fcfcfafbb54a65d2349c6f8419670bf7993d8583?arch=amd64&os=linux&repository_url=localhost%3A5000%2Fscanner-test&tag=unfixed-vulnerabilities-wolfi",
+						"pkg:oci/scanner-test@sha256:a0acdc09bf31ac45076a5850d0564ee144a9c8483f89eb869dc6ddd6218629c9?repository_url=localhost:5000%2Fscanner-test",
+						"pkg:oci/scanner-test@sha256:a0acdc09bf31ac45076a5850d0564ee144a9c8483f89eb869dc6ddd6218629c9?arch=amd64&os=linux&repository_url=localhost:5000%2Fscanner-test&tag=unfixed-vulnerabilities-wolfi",
+						"pkg:oci/scanner-test@sha256:86e3ee69ede5f56514972d16fcfcfafbb54a65d2349c6f8419670bf7993d8583?repository_url=localhost:5000%2Fscanner-test",
+						"pkg:oci/scanner-test@sha256:86e3ee69ede5f56514972d16fcfcfafbb54a65d2349c6f8419670bf7993d8583?arch=amd64&os=linux&repository_url=localhost:5000%2Fscanner-test&tag=unfixed-vulnerabilities-wolfi",
 					},
 				},
 				Hashes: map[vex.Algorithm][]vex.Hash{
@@ -88,8 +88,8 @@ func TestGenerateReferenceIdentifiers(t *testing.T) {
 			expected: IdentifiersBundle{
 				Identifiers: map[vex.IdentifierType][]string{
 					vex.PURL: {
-						"pkg:oci/alpine@sha256%3Affffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff?repository_url=index.docker.io%2Flibrary%2Falpine",
-						"pkg:oci/alpine@sha256%3Affffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff?arch=amd64&os=linux&repository_url=index.docker.io%2Flibrary%2Falpine",
+						"pkg:oci/alpine@sha256:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff?repository_url=index.docker.io%2Flibrary%2Falpine",
+						"pkg:oci/alpine@sha256:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff?arch=amd64&os=linux&repository_url=index.docker.io%2Flibrary%2Falpine",
 					},
 				},
 				Hashes: map[vex.Algorithm][]vex.Hash{
@@ -123,14 +123,14 @@ func TestPurlToReferenceString(t *testing.T) {
 	}{
 		{
 			"normal",
-			"pkg:oci/curl@sha256%3A47fed8868b46b060efb8699dc40e981a0c785650223e03602d8c4493fc75b68c",
+			"pkg:oci/curl@sha256:47fed8868b46b060efb8699dc40e981a0c785650223e03602d8c4493fc75b68c",
 			"curl@sha256:47fed8868b46b060efb8699dc40e981a0c785650223e03602d8c4493fc75b68c",
 			[]RefConverterOptions{},
 			false,
 		},
 		{
 			"normal-with-repo",
-			"pkg:oci/curl@sha256%3A47fed8868b46b060efb8699dc40e981a0c785650223e03602d8c4493fc75b68c?repository_url=cgr.dev/chainguard/curl",
+			"pkg:oci/curl@sha256:47fed8868b46b060efb8699dc40e981a0c785650223e03602d8c4493fc75b68c?repository_url=cgr.dev/chainguard/curl",
 			"cgr.dev/chainguard/curl@sha256:47fed8868b46b060efb8699dc40e981a0c785650223e03602d8c4493fc75b68c",
 			[]RefConverterOptions{},
 			false,
@@ -144,7 +144,7 @@ func TestPurlToReferenceString(t *testing.T) {
 		},
 		{
 			"tag-and-digest",
-			"pkg:oci/debian@sha256%3A47fed8868b46b060efb8699dc40e981a0c785650223e03602d8c4493fc75b68c?tag=latest",
+			"pkg:oci/debian@sha256:47fed8868b46b060efb8699dc40e981a0c785650223e03602d8c4493fc75b68c?tag=latest",
 			"debian@sha256:47fed8868b46b060efb8699dc40e981a0c785650223e03602d8c4493fc75b68c",
 			[]RefConverterOptions{},
 			false,
